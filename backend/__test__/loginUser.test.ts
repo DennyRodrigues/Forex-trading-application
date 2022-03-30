34f97a18f0af5e8117d6ac4cb666c56ecdb53c86
Denny Rodrigues <denny.rodrigues.carmo@gmail.com>
2022-03-30 15:20:43 -0300

    Test user login and get user info on backend

---
+++ backend/__test__/loginUser.test.ts
@@ -0,0 +1,37 @@
+import encodings from "../../node_modules/iconv-lite/encodings";
+import request from "supertest";
+import { app } from "../app";
+import { connectDB } from "../database/connectDB";
+import  MongoClient from "mongodb";
+import User from "../database/models/userModel";
+import bcrypt from "bcrypt";
+
+
+User.findOne = jest.fn().mockReturnValueOnce({
+  name: "test",
+  email: "test@gmail.com",
+  password: 1234,
+  wallet: {
+    USD: 500,
+    GBP: 500,
+  },
+});
+
+
+
+bcrypt.compare = jest.fn().mockReturnValueOnce(true);
+
+
+
+describe("test router /api/v1/users/login", () => {
+  it("Should register User", async () => {
+    const res = await request(app)
+      .post("/api/v1/users/login")
+      .set("Content-type", "application/json")
+      .send({email: "test@gmail.com", password: "1234" })
+      .then((res) => {
+        expect(res.body.result.name).toBe('test');
+        expect(res.statusCode).toBe(200);
+      });
+  });
+});
