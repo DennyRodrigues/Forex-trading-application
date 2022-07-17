bcacbf2f6a379a64a4a4071232fe74c1eaa81165
Denny Carmo <denny.rodrigues.carmo@gmail.com>
2022-07-17 15:18:23 -0300

    Create backend socket and parte sata from the external socket

--- backend/helpers/parseExternalDate.ts
+++ backend/helpers/parseExternalDate.ts
@@ -1,6 +1,5 @@
 export const parseData = (data: Object) => {
-  const response = data.toString(); // Shows the original stringified version
+  const response = JSON.parse(data.toString()); // Shows the original stringified version
   // JSON.parse not working, Get mid value using string split
-  let responseArray = response.split(":");
-  return Number(responseArray[responseArray.length - 1].slice(0, -1));
+  return Number(response.price);
 }
--- backend/socket/webSocketServer.ts
+++ backend/socket/webSocketServer.ts
@@ -1,26 +1,46 @@
+import { config } from "dotenv";
 import express from "express";
 import cors from "cors";
 import ws from "ws";
-import {parseData} from "../helpers/parseExternalDate"
-import ForgeClient from 'forex-quotes';
+import { createServer } from "http";
+import { parseData } from "../helpers/parseExternalDate";
 
-export const startWebScocketServer = () => { 
-const socket = new ws("wss://ws.twelvedata.com/v1/quotes/price?apikey=49b6a79dac694cd0900ab8d8692e367a");
 
-const subscribe = {
-  "action": "subscribe",
-  "params": {
-	"symbols": "AAPL,INFY,TRP,QQQ,IXIC,EUR/USD,USD/JPY,BTC/USD,ETH/BTC"
-  }
-}
-// Connection opened
-socket.addEventListener('open', function (event) {
-    socket.send(JSON.stringify(subscribe));
-});
+export const startWebScocketServer = () => {
+  const port = process.env.PORT_2;
 
-// Listen for messages
-socket.addEventListener('message', function (event) {
-    console.log('Message from server ', event.data);
-});
+  //Start the backend server socket
+  const app = express();
+  app.listen(port, () => {
+    console.log("Websocket Server is up & running", port);
+  });
+  const server = createServer(app);
+  const backendSocket = new ws.Server({ server });
 
-}
+  backendSocket.on("open", function () {
+    console.log("Message from websocket server");
+  });
+
+  // Start external API socket
+  const tradeSocket = new ws(
+    "wss://ws.twelvedata.com/v1/quotes/price?apikey=49b6a79dac694cd0900ab8d8692e367a"
+  );
+
+  // Subscribe message to start getting values
+  const subscribe = {
+    action: "subscribe",
+    params: {
+      symbols: "BTC/USD",
+    },
+  };
+  // Connection opened
+  tradeSocket.addEventListener("open", function (event) {
+    tradeSocket.send(JSON.stringify(subscribe));
+  });
+
+  // Listen for messages and emit it using the backedSocket
+  tradeSocket.addEventListener("message", function (event) {
+    backendSocket.emit("message", () => event.data);
+    console.log("Message from External websocket ", parseData(event.data));
+  });
+};
