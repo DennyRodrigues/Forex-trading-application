2530fa90744b0a67787b35b80a6a9685eadeb70c
Denny Carmo <denny.rodrigues.carmo@gmail.com>
2022-07-22 21:54:28 -0300

    Fix path

--- backend/app.ts
+++ backend/app.ts
@@ -23,9 +23,11 @@ app.use("/api/v1/trades", tradeRouter);
 app.use("/api/v1/users", userRouter);
 
 
-app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
+app.use(express.static(path.join(__dirname, "..", "..", "frontend", "build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "frontend", "build", "index.html"));
+  res.sendFile(
+    path.join(__dirname, "..", "..", "frontend", "build", "index.html")
+  );
 });
 
 // Middlewares
