26e08a1ec91175ce7e754d1acf516e88d28432a7
Denny Carmo <denny.rodrigues.carmo@gmail.com>
2022-07-24 15:39:46 -0300

    use socket URL as env value

--- frontend/src/components/socketcontext/WebSocketProvider.tsx
+++ frontend/src/components/socketcontext/WebSocketProvider.tsx
@@ -5,13 +5,14 @@ import io from "socket.io-client";
 
 // The context will connect to the websocket on backend
 export const WebSocketProvider = (props: any) => {
-  const socketUrl = "ws://localhost:5002/";
+  const socketUrl = process.env.REACT_APP_SOCKET_URL || '';
   // Exchange rate that will be passed 
   const [exchangeRate, setExchangeRate] = useState(0);
 
   useEffect(() => {
     const socket = io(socketUrl)
     socket.on("message", (data) => {
+      console.info(data);
       setExchangeRate(Number(data));
     });
   }, []);
