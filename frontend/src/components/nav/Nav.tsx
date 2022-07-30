36e95392b8ddf493ecbce289cd3df122ed30e5dd
Denny Carmo <denny.rodrigues.carmo@gmail.com>
2022-07-30 13:53:54 -0300

    Change Navbar to be responsive

--- frontend/src/components/nav/Nav.tsx
+++ frontend/src/components/nav/Nav.tsx
@@ -1,29 +1,37 @@
 import { useContext } from "react";
+import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import { AuthContext } from "../../contexts/authentication/AuthContext";
 import { UserInfo } from "../user/UserInfo";
 
-export const Nav = (props: any) => {
+export const NavBarComponent = (props: any) => {
   const onLogout = useContext(AuthContext)?.onLogout;
   const token = useContext(AuthContext)?.token;
 
   if (token) {
     return (
-      <ul className="nav" >
-        <li className="nav-item">
-          <Link to="/" className="nav-link">
-            Trade
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/past" className="nav-link">
+      <Container>
+        <RigthContainer>
+
+      <StyledNavBar collapseOnSelect expand="sm">
+        <StyledToggle aria-controls='responsive-navbar-nav'/>
+        <StyledCollpse id="responsive-navbar-nav">
+          <StyledNav>
+            <StyledLink to="/">
+                  Trade
+            </StyledLink>
+            <StyledLink to="/past">
             Past Trades
-          </Link>
-        </li>
-        <li className="nav-item nav-user-info">
+              </StyledLink>
+          </StyledNav>
+        </StyledCollpse>
+          </StyledNavBar>
+        </RigthContainer>
+        <UserInfoContainer>
           <UserInfo />
-        </li>
-      </ul>
+        </UserInfoContainer>
+      </Container>
     );
   } else {
     return (
@@ -42,3 +50,50 @@ export const Nav = (props: any) => {
     );
   }
 };
+
+
+const Container = styled.div`
+position: fixed;
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+
+width: 100%;
+padding: 1rem 0rem;
+height: 6rem;
+background-color: #796678;
+
+`
+const RigthContainer = styled.div`
+overflow: visible;
+`
+const StyledNavBar = styled(Navbar)`
+overflow: visible;
+background-color: #796678;
+`
+const StyledToggle = styled(Navbar.Toggle)`
+margin-left: 2rem;
+`
+const StyledCollpse = styled(Navbar.Collapse)`
+@media(max-width:576px){
+  padding-bottom: 2rem;
+}
+
+`
+const StyledNav = styled(Nav)`
+`
+const StyledLink = styled(Link)`
+color:#fff;
+text-decoration: none;
+text-transform: uppercase;
+margin-left: 1rem;
+@media(max-width:576px){
+  margin-top: 2rem;
+}
+`
+
+const UserInfoContainer = styled.div`
+  margin-left:auto;
+  margin-right: 1rem;
+  margin-top: 1rem;
+`
