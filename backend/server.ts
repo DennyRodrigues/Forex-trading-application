9cdac15dfbf5fb930bca1bf82be8553472b5e1dd
Unidade <68622249+Unidade@users.noreply.github.com>
2022-11-07 12:52:34 -0300

    Stop WebSocketServer

--- backend/server.ts
+++ backend/server.ts
@@ -3,14 +3,14 @@ config()
 import { createServer } from 'http'
 import { app } from './app'
 import { connectDB } from './database/connectDB'
-import { setupWebSocketServer } from './socket/setupWebSocketServer'
+// import { setupWebSocketServer } from './socket/setupWebSocketServer'
 
 const port = process.env.PORT
 const server = createServer(app)
 
 // Initialize the startup routine
 connectDB().then(() => {
-  setupWebSocketServer(server)
+  // setupWebSocketServer(server)
   app.listen(port, () => console.log('Server is up and running on port:', port))
   console.log(app)
 })
