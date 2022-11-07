c8f28466c2280bce342fc798633326ee84923826
Unidade <68622249+Unidade@users.noreply.github.com>
2022-11-07 15:09:31 -0300

    Remove unused files and lines

--- backend/server.ts
+++ backend/server.ts
@@ -2,13 +2,11 @@ import { config } from 'dotenv'
 config()
 import { app } from './app'
 import { connectDB } from './database/connectDB'
-// import { setupWebSocketServer } from './socket/setupWebSocketServer'
 
 const port = process.env.PORT
 
 // Initialize the startup routine
 connectDB().then(() => {
-  // setupWebSocketServer(server)
   app.listen(port, () => console.log('Server is up and running on port:', port))
   console.log(app)
 })
